daa68bc550c6cd4d5dc0604880fc9a8b975c2eaf
akella <akella.a@gmail.com>
2014-10-03 17:10:54 +0200

    profile done

--- js/common.js
+++ js/common.js
@@ -49,4 +49,42 @@ head.ready(function() {
 	$('.input__currency_drop').click(function(event) {
 		$(this).toggleClass('is-opendrop');
 	});
+
+	$(document).on("scroll", onScroll);
+
+	//smoothscroll
+	$('a[href^="#"]').on('click', function (e) {
+	    e.preventDefault();
+	    $(document).off("scroll");
+	    
+	    $('.settings__nav a').each(function () {
+	        $(this).removeClass('is-active');
+	    })
+	    $(this).addClass('is-active');
+	  
+	    var target = this.hash,
+	        menu = target;
+	    $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top-180
+	    }, 500, 'swing', function () {
+	        window.location.hash = target;
+	        $(document).on("scroll", onScroll);
+	    });
+	});
+
+	function onScroll(event){
+	    var scrollPos = $(document).scrollTop();
+	    $('.settings__nav a').each(function () {
+	        var currLink = $(this);
+	        var refElement = $(currLink.attr("href"));
+	        if (refElement.position().top - 200 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	            $('.settings__nav a').removeClass("is-active");
+	            currLink.addClass("is-active");
+	        }
+	        else{
+	            currLink.removeClass("is-active");
+	        }
+	    });
+	}
 });
